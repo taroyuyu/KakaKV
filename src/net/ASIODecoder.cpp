acfab77eecdad4a5eae980b94af0e3c71f651e13
lingyu <891214416@qq.com>
2021-09-29 15:35:20 +0800

    添加基于ASIO和Protobuf的消息编解码器

---
+++ src/net/ASIODecoder.cpp
@@ -0,0 +1,14 @@
+//
+// Created by 凌宇 on 2021/9/29.
+//
+
+#include <net/ASIODecoder.h>
+
+namespace kakakv {
+    namespace net {
+        bool
+        ASIODecoder::tryToretriveMessage(::google::protobuf::Message **message) const{
+
+        }
+    }
+}
