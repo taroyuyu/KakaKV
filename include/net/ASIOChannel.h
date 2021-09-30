d43899bd11db019a55089382cf4f99546025ec1b
lingyu <891214416@qq.com>
2021-09-30 14:35:06 +0800

    完善ASIOChannel

--- include/net/ASIOChannel.h
+++ include/net/ASIOChannel.h
@@ -15,34 +15,53 @@
 #include <message/AppendEntriesResponse.h>
 #include <common/net/buffer/CircleBuffer.h>
 #include <mutex>
-namespace kakakv{
-    namespace net{
+
+namespace kakakv {
+    namespace net {
         class Selector;
-    class ASIOChannel:public Channel,private std::enable_shared_from_this<ASIOChannel>{
+
+        class ASIOChannel : public Channel, private std::enable_shared_from_this<ASIOChannel> {
         public:
             friend Selector;
-            ASIOChannel(std::unique_ptr<boost::asio::ip::tcp::socket> socket,std::shared_ptr<Decoder> decoder,std::shared_ptr<Encoder> encoder);
-            void addHandler(std::shared_ptr<AbstractHandler> handler)override;
-            void removeHandler(std::shared_ptr<AbstractHandler> handler)override;
-            void writeMessage(std::shared_ptr<const Message> message)override;
+
+            ASIOChannel(std::unique_ptr<boost::asio::ip::tcp::socket> socket, std::shared_ptr<Decoder> decoder,
+                        std::shared_ptr<Encoder> encoder);
+
+            void addHandler(std::shared_ptr<AbstractHandler> handler) override;
+
+            void removeHandler(std::shared_ptr<AbstractHandler> handler) override;
+
+            void writeMessage(std::shared_ptr<const Message> message) override;
+
             // 发送RequestVote消息
             void writeRequestVote(const std::shared_ptr<const message::RequestVote> message);
+
             // 发送RequestVoteResponse消息
             void writeRequestVoteResponse(const std::shared_ptr<const message::RequestVoteResponse> message);
+
             // 发送AppendEntries消息
             void writeAppendEntries(const std::shared_ptr<const message::AppendEntries> message);
+
             // 发送AppendEntriesResponse消息
             void writeAppendEntriesResponse(const std::shared_ptr<const message::AppendEntriesResponse> message);
+
             // 关闭
-            void close()override;
+            void close() override;
+
             // 添加Close回掉函数
-            void addCloseCallback(std::function<void(std::shared_ptr<Channel> channel)> callback)override;
+            void addCloseCallback(std::function<void(std::shared_ptr<Channel> channel)> callback) override;
+
         private:
+            void sendByteStream(std::shared_ptr<common::net::CircleBuffer> buffer);
+            std::mutex mMutex;
             const std::unique_ptr<boost::asio::ip::tcp::socket> mSocket;
+            std::unique_ptr<char[]> mTmpInputBuffer;
+            unsigned long mTmpInputBufferLength;
+            std::unique_ptr<char[]> mTmpoutputBuffer;
+            unsigned long mTmpOutputBufferLength;
             std::set<std::shared_ptr<AbstractHandler>> mHandlerSet;
             std::shared_ptr<Decoder> mDecoder;
             std::shared_ptr<Encoder> mEncoder;
-            std::mutex mCodecBufferMutex;
             const std::shared_ptr<common::net::CircleBuffer> mCodecBuffer;
         };
     }
--- src/net/ASIOChannel.cpp
+++ src/net/ASIOChannel.cpp
@@ -6,54 +6,63 @@
 #include <net/serialization/KakaKVRaftMessage.pb.h>
 #include <log/GeneralLogEntry.h>
 #include <log/NoOpLogEntry.h>
-namespace kakakv{
-    namespace net{
-        ASIOChannel::ASIOChannel(std::unique_ptr<boost::asio::ip::tcp::socket> socket,std::shared_ptr<Decoder> decoder,std::shared_ptr<Encoder> encoder):
-        Channel(decoder,encoder),mSocket(std::move(socket)),mDecoder(decoder),mEncoder(encoder),mCodecBuffer(std::make_shared<common::net::CircleBuffer>(1024)){
+
+namespace kakakv {
+    namespace net {
+        ASIOChannel::ASIOChannel(std::unique_ptr<boost::asio::ip::tcp::socket> socket, std::shared_ptr<Decoder> decoder,
+                                 std::shared_ptr<Encoder> encoder) :
+                Channel(decoder, encoder), mSocket(std::move(socket)), mDecoder(decoder), mEncoder(encoder),
+                mCodecBuffer(std::make_shared<common::net::CircleBuffer>(1024)), mTmpInputBufferLength(0),
+                mTmpOutputBufferLength(0) {
 
         }
 
-        void ASIOChannel::addHandler(std::shared_ptr<AbstractHandler> handler){
+        void ASIOChannel::addHandler(std::shared_ptr<AbstractHandler> handler) {
             auto it = this->mHandlerSet.find(handler);
             if (it != this->mHandlerSet.end()) {
                 return;
             }
             this->mHandlerSet.insert(handler);
         }
-        void ASIOChannel::removeHandler(std::shared_ptr<AbstractHandler> handler){
+
+        void ASIOChannel::removeHandler(std::shared_ptr<AbstractHandler> handler) {
             auto it = this->mHandlerSet.find(handler);
             if (it == this->mHandlerSet.end()) {
                 return;
             }
             this->mHandlerSet.erase(it);
         }
-        void ASIOChannel::writeMessage(std::shared_ptr<const Message> message){
-            if (!message){
+
+        void ASIOChannel::writeMessage(std::shared_ptr<const Message> message) {
+            if (!message) {
                 return;
             }
-            if (typeid(message.get()) == typeid(const kakakv::message::RequestVote*)){
+            if (typeid(message.get()) == typeid(const kakakv::message::RequestVote *)) {
                 auto requestVoteMessage = std::dynamic_pointer_cast<const kakakv::message::RequestVote>(message);
                 assert(requestVoteMessage);
                 this->writeRequestVote(requestVoteMessage);
-            }else if (typeid(message.get()) == typeid(const kakakv::message::RequestVoteResponse*)){
-                auto requestVoteResponseMessage = std::dynamic_pointer_cast<const kakakv::message::RequestVoteResponse>(message);
+            } else if (typeid(message.get()) == typeid(const kakakv::message::RequestVoteResponse *)) {
+                auto requestVoteResponseMessage = std::dynamic_pointer_cast<const kakakv::message::RequestVoteResponse>(
+                        message);
                 assert(requestVoteResponseMessage);
                 this->writeRequestVoteResponse(requestVoteResponseMessage);
-            }else if (typeid(message.get()) == typeid(const kakakv::message::AppendEntries*)){
+            } else if (typeid(message.get()) == typeid(const kakakv::message::AppendEntries *)) {
                 auto appendEntriesMessage = std::dynamic_pointer_cast<const kakakv::message::AppendEntries>(message);
                 assert(appendEntriesMessage);
                 this->writeAppendEntries(appendEntriesMessage);
-            }else if (typeid(message.get()) == typeid(const kakakv::message::AppendEntriesResponse*)){
-                auto appendEntriesResponseMessage = std::dynamic_pointer_cast<const kakakv::message::AppendEntriesResponse>(message);
+            } else if (typeid(message.get()) == typeid(const kakakv::message::AppendEntriesResponse *)) {
+                auto appendEntriesResponseMessage = std::dynamic_pointer_cast<const kakakv::message::AppendEntriesResponse>(
+                        message);
                 assert(appendEntriesResponseMessage);
                 this->writeAppendEntriesResponse(appendEntriesResponseMessage);
-            }else{
+            } else {
                 //未知类型
                 assert(false);
             }
         }
+
         // 发送RequestVote消息
-        void ASIOChannel::writeRequestVote(const std::shared_ptr<const kakakv::message::RequestVote> message){
+        void ASIOChannel::writeRequestVote(const std::shared_ptr<const kakakv::message::RequestVote> message) {
             //1. 将消息封装成Protobuf格式
             auto requestVoteMessage = std::make_shared<message::RequestVoteMessage>();
             requestVoteMessage->set_term(message->term);
@@ -62,28 +71,31 @@ namespace kakakv{
             requestVoteMessage->set_lastlogindex(message->lastLogIndex);
             assert(requestVoteMessage->IsInitialized());
             //2. 将消息封装成字节流
-            {
-                std::lock_guard<std::mutex> lock(this->mCodecBufferMutex);
-                assert(this->mCodecBuffer->getUsed() == 0);
-                this->mEncoder->encapsulateMessageToByteStream(*requestVoteMessage,this->mCodecBuffer);
-            }
+            std::lock_guard<std::mutex> lock(this->mMutex);
+            assert(this->mCodecBuffer->getUsed() == 0);
+            this->mEncoder->encapsulateMessageToByteStream(*requestVoteMessage, this->mCodecBuffer);
+            //3. 发送字节流
+            this->sendByteStream(this->mCodecBuffer);
         }
+
         // 发送RequestVoteResponse消息
-        void ASIOChannel::writeRequestVoteResponse(const std::shared_ptr<const kakakv::message::RequestVoteResponse> message){
+        void ASIOChannel::writeRequestVoteResponse(
+                const std::shared_ptr<const kakakv::message::RequestVoteResponse> message) {
             //1. 将消息封装成Protobuf格式
             auto requestVoteResponseMessage = std::make_shared<message::RequestVoteResponseMessage>();
             requestVoteResponseMessage->set_term(message->term);
             requestVoteResponseMessage->set_votegranted(message->voteGranted);
             assert(requestVoteResponseMessage->IsInitialized());
             //2. 将消息封装成字节流
-            {
-                std::lock_guard<std::mutex> lock(this->mCodecBufferMutex);
-                assert(this->mCodecBuffer->getUsed() == 0);
-                this->mEncoder->encapsulateMessageToByteStream(*requestVoteResponseMessage,this->mCodecBuffer);
-            }
+            std::lock_guard<std::mutex> lock(this->mMutex);
+            assert(this->mCodecBuffer->getUsed() == 0);
+            this->mEncoder->encapsulateMessageToByteStream(*requestVoteResponseMessage, this->mCodecBuffer);
+            //3. 发送字节流
+            this->sendByteStream(this->mCodecBuffer);
         }
+
         // 发送AppendEntries消息
-        void ASIOChannel::writeAppendEntries(const std::shared_ptr<const kakakv::message::AppendEntries> message){
+        void ASIOChannel::writeAppendEntries(const std::shared_ptr<const kakakv::message::AppendEntries> message) {
             //1. 将消息封装成Protobuf格式
             auto appendEntriesMessage = std::make_shared<message::AppendEntriesMessage>();
             appendEntriesMessage->set_term(message->term);
@@ -91,64 +103,78 @@ namespace kakakv{
             appendEntriesMessage->set_prevlogterm(message->prevLogTerm);
             appendEntriesMessage->set_prevlogindex(message->prevLogIndex);
             appendEntriesMessage->set_leadercommit(message->leaderCommitIndex);
-            for(auto entry : message->entryList){
+            for (auto entry: message->entryList) {
                 auto entryItem = appendEntriesMessage->add_entrylist();
                 entryItem->set_index(entry->getIndex());
                 entryItem->set_term(entry->getTerm());
-                if (typeid(entry.get()) == typeid(log::GeneralLogEntry*)){
+                if (typeid(entry.get()) == typeid(log::GeneralLogEntry *)) {
                     entryItem->set_kind(message::AppendEntriesMessage_Entry_Kind_GeneralLog);
                     auto generalLog = std::dynamic_pointer_cast<log::GeneralLogEntry>(entry);
                     assert(generalLog);
                     std::string data(generalLog->getPayload().first, generalLog->getPayload().second);
                     entryItem->set_data(data);
-                }else if (typeid(entry.get()) == typeid(log::NoOpLogEntry*)){
+                } else if (typeid(entry.get()) == typeid(log::NoOpLogEntry *)) {
                     entryItem->set_kind(message::AppendEntriesMessage_Entry_Kind_NoOpLog);
-                }else{
+                } else {
                     assert(false);
                     continue;
                 }
             }
             assert(appendEntriesMessage->IsInitialized());
             //2. 将消息封装成字节流
-            {
-                std::lock_guard<std::mutex> lock(this->mCodecBufferMutex);
-                assert(this->mCodecBuffer->getUsed() == 0);
-                this->mEncoder->encapsulateMessageToByteStream(*appendEntriesMessage,this->mCodecBuffer);
-            }
+            std::lock_guard<std::mutex> lock(this->mMutex);
+            assert(this->mCodecBuffer->getUsed() == 0);
+            this->mEncoder->encapsulateMessageToByteStream(*appendEntriesMessage, this->mCodecBuffer);
+            //3. 发送字节流
+            this->sendByteStream(this->mCodecBuffer);
         }
+
         // 发送AppendEntriesResponse消息
-        void ASIOChannel::writeAppendEntriesResponse(const std::shared_ptr<const kakakv::message::AppendEntriesResponse> message){
+        void ASIOChannel::writeAppendEntriesResponse(
+                const std::shared_ptr<const kakakv::message::AppendEntriesResponse> message) {
             //1. 将消息封装成Protobuf格式
             auto appendEntriesResponseMessage = std::make_shared<message::AppendEntriesResponseMessage>();
             appendEntriesResponseMessage->set_term(message->term);
             appendEntriesResponseMessage->set_success(message->success);
             assert(appendEntriesResponseMessage->IsInitialized());
             //2. 将消息封装成字节流
-            {
-                std::lock_guard<std::mutex> lock(this->mCodecBufferMutex);
-                assert(this->mCodecBuffer->getUsed() == 0);
-                this->mEncoder->encapsulateMessageToByteStream(*appendEntriesResponseMessage,this->mCodecBuffer);
-            }
+            std::lock_guard<std::mutex> lock(this->mMutex);
+            assert(this->mCodecBuffer->getUsed() == 0);
+            this->mEncoder->encapsulateMessageToByteStream(*appendEntriesResponseMessage, this->mCodecBuffer);
+            //3. 发送字节流
+            this->sendByteStream(this->mCodecBuffer);
         }
+
+        void ASIOChannel::sendByteStream(std::shared_ptr<common::net::CircleBuffer> buffer){
+            //1. 判断是否需要更新临时输出缓冲区
+            if (buffer->getUsed() > this->mTmpOutputBufferLength){
+                this->mTmpOutputBufferLength = buffer->getUsed();
+                this->mTmpoutputBuffer.reset(new char[mTmpOutputBufferLength]);
+            }
+            //2. 将字节流放入临时输出缓冲区中
+            buffer->retrive(mTmpoutputBuffer.get(),mTmpOutputBufferLength);
+        }
+
         // 关闭
-        void ASIOChannel::close(){
+        void ASIOChannel::close() {
             //1. 关闭连接
             boost::system::error_code ec;
-            this->mSocket->shutdown(boost::asio::ip::tcp::socket::shutdown_both,ec);
-            if (ec){
+            this->mSocket->shutdown(boost::asio::ip::tcp::socket::shutdown_both, ec);
+            if (ec) {
 
             }
             this->mSocket->close(ec);
-            if (ec){
+            if (ec) {
 
             }
             //2. 执行Close回掉函数
-            for (auto callback : this->closeCallbackList){
+            for (auto callback: this->closeCallbackList) {
                 callback(this->shared_from_this());
             }
         }
+
         // 添加Close回掉函数
-        void ASIOChannel::addCloseCallback(std::function<void(std::shared_ptr<Channel> channel)> callback){
+        void ASIOChannel::addCloseCallback(std::function<void(std::shared_ptr<Channel> channel)> callback) {
             this->closeCallbackList.push_back(callback);
         }
     }
